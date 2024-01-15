eb81a211a58d8b3fc8f7e61c36a815a71f43e416
Dispenser254 <stevensonkibs55@gmail.com>
2024-01-15 23:44:33 +0300

    Updated the dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,21 @@
 
 import { Badge, Datepicker, Table } from "flowbite-react";
 import { NavbarSidebar } from "../layouts/NavbarSidebar";
-import { HiArrowSmallRight, HiArrowTrendingUp, HiChevronRight } from "react-icons/hi2";
+import {
+	HiArrowSmallRight,
+	HiArrowTrendingUp,
+	HiChevronRight,
+} from "react-icons/hi2";
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+} from "recharts";
 
 export const Dashboard = () => {
 	return (
@@ -45,7 +59,9 @@ function NewProductsThisWeek() {
 		<div className="rounded-lg bg-white p-4 shadow dark:bg-gray-800 sm-p-6 xl:p-8">
 			<div className="flex items-center">
 				<div className="shrink-0">
-					<span className="text-2xl font-bold leading-none text-gray-900 dark:text-white sm:text-3xl">2,340</span>
+					<span className="text-2xl font-bold leading-none text-gray-900 dark:text-white sm:text-3xl">
+						2,340
+					</span>
 					<h3 className="text-base font-normal text-gray-600 dark:text-gray-400">
 						New products this week
 					</h3>
@@ -54,8 +70,96 @@ function NewProductsThisWeek() {
 					<span className="p-1">14.6%</span> <HiArrowTrendingUp size={20} />
 				</div>
 			</div>
+			<NewProductsChart />
+			<div className="flex items-center justify-between border-t border-gray-200 pt-3 dark:border-gray-700 sm:pt-6">
+				<div className="shrink-0">
+					<a
+						href="#"
+						className="inline-flex items-center rounded-lg p-2 text-xs font-medium uppercase text-primary-700 hover:bg-gray-100 dark:text-primary-500 dark:hover:bg-gray-700 sm:text-sm"
+					>
+						<span className="p-2"> Products Report</span>
+						<HiArrowSmallRight className="translate-y-2" size={20} />
+					</a>
+				</div>
+			</div>
 		</div>
-	)
+	);
+}
+
+const data = [
+		{
+			name: "Page A",
+			uv: 4000,
+			pv: 2400,
+			amt: 2400,
+		},
+		{
+			name: "Page B",
+			uv: 3000,
+			pv: 1398,
+			amt: 2210,
+		},
+		{
+			name: "Page C",
+			uv: 2000,
+			pv: 9800,
+			amt: 2290,
+		},
+		{
+			name: "Page D",
+			uv: 2780,
+			pv: 3908,
+			amt: 2000,
+		},
+		{
+			name: "Page E",
+			uv: 1890,
+			pv: 4800,
+			amt: 2181,
+		},
+		{
+			name: "Page F",
+			uv: 2390,
+			pv: 3800,
+			amt: 2500,
+		},
+		{
+			name: "Page G",
+			uv: 3490,
+			pv: 4300,
+			amt: 2100,
+		},
+	];
+
+function NewProductsChart() {
+	return (
+		<ResponsiveContainer maxHeight={200} width="100%" height="100%">
+			<LineChart
+				width={500}
+				height={200}
+				data={data}
+				margin={{
+					top: 5,
+					right: 30,
+					left: 20,
+					bottom: 5,
+				}}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="name" />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Line
+					type="monotone"
+					dataKey="pv"
+					stroke="#8884d8"
+					activeDot={{ r: 8 }}
+				/>
+				<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+			</LineChart>
+		</ResponsiveContainer>
+	);
 }
 
 function Transactions() {
